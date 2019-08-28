e3a1b75c5c5aed607e68f5ace027f70d026ba3d2
Thinhinane Ihadadene <et_ihadadene@esi.dz>
2019-08-29 00:40:06 +0100

    add measures

--- benchmarks/automatic_comm/wrapper_nnconv.cpp
+++ benchmarks/automatic_comm/wrapper_nnconv.cpp
@@ -14,19 +14,22 @@ int main() {
 
     Halide::Buffer<float> input(_FIN, _N, _N, _BATCH_SIZE/_NODES + 2, "input");
 
-    Halide::Buffer<float> input_g(_FIN, _N, _N, _BATCH_SIZE, "input");
+    std::vector<std::chrono::duration<double,std::milli>> duration_vector_1;
 
-    for(int i = 0; i < _BATCH_SIZE; i++)
-            for(int k = 0; k < _N; k++)
-                for(int l=0; l < _N; l++)
-                    for(int m=0; m < _FIN; m++)
-                        input_g(m,k,l,i) = k+l+m;
+    // Halide::Buffer<float> input_g(_FIN, _N, _N, _BATCH_SIZE, "input");
+
+    // for(int i = 0; i < _BATCH_SIZE; i++)
+    //         for(int k = 0; k < _N; k++)
+    //             for(int l=0; l < _N; l++)
+    //                 for(int m=0; m < _FIN; m++)
+    //                     input_g(m,k,l,i) = k+l+m;
 
                         for(int i = 0; i < _BATCH_SIZE/_NODES; i++)
                                 for(int k = 0; k < _N; k++)
                                     for(int l=0; l < _N; l++)
                                         for(int m=0; m < _FIN; m++)
-                                            input(m,l,k,i) = input_g(m,l,k,i + rank * _BATCH_SIZE/_NODES);
+                                            // input(m,l,k,i) = input_g(m,l,k,i + rank * _BATCH_SIZE/_NODES);
+                                            input(m,k,l,i) = k+l+m;
 
     Halide::Buffer<float> bias(_FOUT_BLOCKING,  _FOUT_NB_BLOCKS, "bias");
 
@@ -34,7 +37,7 @@ int main() {
 
     Halide::Buffer<float> output(_FOUT_BLOCKING, _N, _N, _FOUT_NB_BLOCKS,_BATCH_SIZE/_NODES, "output");
 
-    Halide::Buffer<float> output_g(_FOUT_BLOCKING, _N, _N, _FOUT_NB_BLOCKS,_BATCH_SIZE, "output");
+    // Halide::Buffer<float> output_g(_FOUT_BLOCKING, _N, _N, _FOUT_NB_BLOCKS,_BATCH_SIZE, "output");
 
 
   for (int fout = 0; fout < _FOUT; ++fout)
@@ -48,20 +51,36 @@ int main() {
       bias(fout, ffout) = 8;
 
 
-      MPI_Barrier(MPI_COMM_WORLD);
-      nnconv_tiramisu(input.raw_buffer(),bias.raw_buffer(),filter.raw_buffer(), output.raw_buffer());
-      nnconv_ref(input_g.raw_buffer(),bias.raw_buffer(),filter.raw_buffer(), output_g.raw_buffer());
-      MPI_Barrier(MPI_COMM_WORLD);
+      for(int i = 0; i < 3; i++)
+      {
+          MPI_Barrier(MPI_COMM_WORLD);
+          auto start1 = std::chrono::high_resolution_clock::now();
 
-    for (int n = 0; n < _BATCH_SIZE/_NODES; ++n)
-    	for (int fout = 0; fout < _FOUT_NB_BLOCKS; ++fout)
-    		for (int y = 0; y < _N; ++y)
-    			for (int x = 0; x < _N; ++x)
-    				for (int ffout = 0; ffout <_FOUT_BLOCKING; ++ffout)
-                        if(output_g(ffout, x, y, fout, n) != output(ffout, x, y, fout, n))
-                        {
-                            std::cout << "ERROR at " << rank << "test" << n <<std::endl;
-                        }
+          nnconv_tiramisu(input.raw_buffer(),bias.raw_buffer(),filter.raw_buffer(), output.raw_buffer());
+
+          auto end1 = std::chrono::high_resolution_clock::now();
+
+          std::chrono::duration<double,std::milli> duration1 = end1 - start1;
+          duration_vector_1.push_back(duration1);
+      }
+
+      // nnconv_ref(input_g.raw_buffer(),bias.raw_buffer(),filter.raw_buffer(), output_g.raw_buffer());
+      MPI_Barrier(MPI_COMM_WORLD);
+      if(rank == 0)
+      {    print_time("performance_CPU.csv", "nnconv",
+              {"Tiramisu auto", "Tiramisu man"},
+           {median(duration_vector_1), 0});
+      }
+    //
+    // for (int n = 0; n < _BATCH_SIZE/_NODES; ++n)
+    // 	for (int fout = 0; fout < _FOUT_NB_BLOCKS; ++fout)
+    // 		for (int y = 0; y < _N; ++y)
+    // 			for (int x = 0; x < _N; ++x)
+    // 				for (int ffout = 0; ffout <_FOUT_BLOCKING; ++ffout)
+    //                     if(output_g(ffout, x, y, fout, n) != output(ffout, x, y, fout, n))
+    //                     {
+    //                         std::cout << "ERROR at " << rank << "test" << n <<std::endl;
+    //                     }
 
   tiramisu_MPI_cleanup();
 #endif
